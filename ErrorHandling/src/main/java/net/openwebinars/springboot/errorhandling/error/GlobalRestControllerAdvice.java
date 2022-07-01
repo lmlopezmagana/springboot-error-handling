991556be4318e7ba2e59a25d7984a142f6192e9f
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-07-01 12:01:00 +0200

    Gestión centralizada de errores

---
+++ ErrorHandling/src/main/java/net/openwebinars/springboot/errorhandling/error/GlobalRestControllerAdvice.java
@@ -0,0 +1,82 @@
+package net.openwebinars.springboot.errorhandling.error;
+
+import net.openwebinars.springboot.errorhandling.error.model.impl.ApiErrorImpl;
+import net.openwebinars.springboot.errorhandling.error.model.impl.ApiValidationSubError;
+import net.openwebinars.springboot.errorhandling.exception.EmptyNoteListException;
+import net.openwebinars.springboot.errorhandling.exception.NoteNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+
+@RestControllerAdvice
+public class GlobalRestControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return buildApiError(ex.getMessage(), request, status);
+    }
+
+
+    @ExceptionHandler({NoteNotFoundException.class, EmptyNoteListException.class})
+    public ResponseEntity<?> handleNotFoundException(EntityNotFoundException exception, WebRequest request) {
+        return buildApiError(exception.getMessage(), request, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return buildApiError("Error on marshalling / unmarshalling of a JSON object: " + ex.getMessage(), request, status);
+
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        //return super.handleMethodArgumentNotValid(ex, headers, status, request);
+        return buildApiErrorWithSubErrors("Validation error. Please check the sublist.", request, status, ex.getAllErrors());
+    }
+
+    private final ResponseEntity<Object> buildApiError(String message, WebRequest request, HttpStatus status) {
+        return ResponseEntity
+                .status(status)
+                .body(
+                        ApiErrorImpl.builder()
+                                .status(status)
+                                .message(message)
+                                .path(((ServletWebRequest) request).getRequest().getRequestURI())
+                                .build()
+                );
+    }
+
+    private final ResponseEntity<Object> buildApiErrorWithSubErrors(String message, WebRequest request, HttpStatus status, List<ObjectError> subErrors) {
+        return ResponseEntity
+                .status(status)
+                .body(
+                        ApiErrorImpl.builder()
+                                .status(status)
+                                .message(message)
+                                .path(((ServletWebRequest) request).getRequest().getRequestURI())
+                                .subErrors(subErrors.stream()
+                                            .map(ApiValidationSubError::fromObjectError)
+                                            .collect(Collectors.toList())
+                                )
+                                .build()
+                );
+
+    }
+
+}
